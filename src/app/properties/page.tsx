ef480a752d4b7ae20f6505d88ebaabb1d3ec499b
Arvin Buid <arvinbuid01@gmail.com>
2025-05-11 10:27:59 +0800

    Created properties.json file as dummy data and PropertiesPage component.

---
+++ src/app/properties/page.tsx
@@ -0,0 +1,11 @@
+const PropertiesPage = () => {
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container mx-auto px-4 py-6">
+
+            </div>
+        </section>
+    )
+}
+
+export default PropertiesPage
