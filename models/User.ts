d546b8886d11a2c17dd70f77add7552174dc5147
Arvin Buid <arvinbuid01@gmail.com>
2025-05-12 21:44:57 +0800

    Created User & Property models.

---
+++ models/User.ts
@@ -0,0 +1,31 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, "Email already exists"],
+      required: [true, "Email is required"],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Property",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
