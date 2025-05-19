8e05585caf8e75db5164882384b4bc6268e6be77
Arvin Buid <arvinbuid01@gmail.com>
2025-05-19 09:34:05 +0800

    Build ShareButtons component functionality.

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -1,15 +1,34 @@
-import { FaShare } from "react-icons/fa";
+'use client'
+
 import { PropertyType } from "../../types/property";
+import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon, WhatsappShareButton, WhatsappIcon, LinkedinShareButton, LinkedinIcon, } from "react-share";
 
 interface ShareButtonsProps {
     property: PropertyType;
 }
 
 const ShareButtons = ({ property }: ShareButtonsProps) => {
+    const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+
     return (
-        <button className="bg-orange-500 hover:bg-orange-600 transition-colors text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-            <FaShare className="mr-2" /> Share Property
-        </button>
+        <>
+            <h3 className="text-center font-bold pt-2 text-lg">Share This Property:</h3>
+            <div className="flex justify-center items-center gap-4">
+                <FacebookShareButton url={shareUrl} hashtag={`#${property.type.replace('/\s/g', '')}ForRent.`}>
+                    <FacebookIcon size={40} round={true} />
+                </FacebookShareButton >
+                <TwitterShareButton url={shareUrl} title={property.name} hashtags={[`${property.type.replace('/\s/g', '')}ForRent🏢`]}>
+                    <TwitterIcon size={40} round={true} />
+                </TwitterShareButton>
+                <WhatsappShareButton url={shareUrl} title={property.name} separator="::">
+                    <WhatsappIcon size={40} round={true} />
+                </WhatsappShareButton>
+                <LinkedinShareButton url={shareUrl} title={property.name} summary={property.description}>
+                    <LinkedinIcon size={40} round={true} />
+                </LinkedinShareButton>
+
+            </div>
+        </>
     );
 }
 
