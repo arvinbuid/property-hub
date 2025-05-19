5760e54e6a45e864c5b653fa26f6bb628b690199
Arvin Buid <arvinbuid01@gmail.com>
2025-05-19 17:43:02 +0800

    Created Message model and addMessage server action.

---
+++ src/app/actions/addMessage.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import connectDB from "../../../config/database";
+import Message from "../../../models/Message";
+
+import {getSessionUser} from "../../../utils/getSessionUser";
+
+const addMessage = async (formData: FormData) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      throw new Error("User not found.");
+    }
+
+    const {userId} = sessionUser;
+
+    const recipient = formData.get("recipient");
+
+    if (userId === recipient) throw new Error("You cannot send a message to yourself.");
+
+    const newMessage = new Message({
+      owner: userId,
+      recipient,
+      property: formData.get("property"),
+      name: formData.get("name"),
+      email: formData.get("email"),
+      phone: formData.get("phone"),
+      body: formData.get("body"),
+    });
+
+    await newMessage.save();
+
+    return {
+      submitted: true,
+      message: "Message sent successfully.",
+    };
+  } catch (error) {
+    return {
+      submitted: false,
+      message: `Error sending message: ${error}`,
+    };
+  }
+};
+
+export default addMessage;
